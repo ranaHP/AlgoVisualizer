f897338c59737999c068ac73873e51b7f95d4723
R_Hansana <hansana.ranaweera@taprotechnologies.com>
2024-07-03 22:16:41 +0530

    FEA | F002 | algo selector component done

--- src/app/components/algoSelector.component.tsx
+++ src/app/components/algoSelector.component.tsx
@@ -1,7 +1,19 @@
+import { useAlgoVisualizerContext } from "../context/visualizer";
+import { AlgoType } from "../lib/type";
+
 const AlgoSelectorComponent = (props:any) => {
+    const {setSelectedAlgo} = useAlgoVisualizerContext();
     return(
-        <div className="bg-slate-100 w-fit">
-           AlgoSelector
+        <div className="bg-slate-50 rounded-md text-center flex justify-center items-center px-3 w-fit ">
+            <select name="algo" id="algo" className="bg-slate-50 rounded-md text-center px-4  text-sm cursor-pointer"
+            onChange={(e) => setSelectedAlgo((e.target.value) as AlgoType)}> 
+                <option className="w-[150px] cursor-pointer" value="bubble">BubbleSort</option>
+                <option value="selection">SelectionSort</option>
+                <option value="insertioin">InsertionSort</option>
+                <option value="HeapSort">HeapSort</option>
+                <option value="merge">MergeSort</option>
+                <option value="quick">QuickSort</option>
+            </select>
         </div>
     )
 }
